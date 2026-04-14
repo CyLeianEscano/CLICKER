9336b31dac2dad754e8d7f8af6d5dbdd3cc14111
CyLeianEscano <cyleian.escano@antiquespride.edu.ph>
2026-04-15 07:10:54 +0800

    update db to supabase

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
 
     // Replace with your Supabase project URL and anon key
-    define('SUPABASE_URL', 'https://your-project.supabase.co');
-    define('SUPABASE_ANON_KEY', 'your-anon-key');
+    define('SUPABASE_URL', 'https://bsjwmnpwdjbuijzcetpa.supabase.co');
+    define('SUPABASE_ANON_KEY', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJzandtbnB3ZGpidWlqemNldHBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxODUxNzksImV4cCI6MjA5MTc2MTE3OX0.kpQvbFbDNuyt_2R400KveVTdx09qEbJakWcS8sx7XTI');
 
 ?>
